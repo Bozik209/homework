d3e4524980a6f332d711d984f593a3d5afc33682
boaz209 <44434303+boaz209@users.noreply.github.com>
2018-11-11 14:09:15 +0200

    Add files via upload

---
+++ Matrix.HW.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row= 3,col=3;
+            string[][] matrix = new string[3][];
+            matrix[0] = new string[3] { "Main","up","sub" };
+            matrix[1] = new string[3] { "down", "center","up"};
+            matrix[2] = new string[3] { "sub","down","main" };
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    if (i==j)
+                    {
+                        Console.Write($"{matrix[i][j]}      ");
+                    }
+                    if (i<j)
+                    {
+                        Console.Write($"{matrix[i][j]}      ");
+                    }
+                    if (i>j)
+                    {
+                        Console.Write($"{matrix[i][j]}      ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
