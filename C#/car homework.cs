c3520efb6db523b898518c8eca8374f2d7185908
boaz209 <44434303+boaz209@users.noreply.github.com>
2018-11-25 16:42:28 +0200

    Add files via upload

---
+++ C#/car homework.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice = 0;
+            //2.1
+            car car1=new car();
+            car1.color="Red";
+            car1.model = "Toyota";
+            car1.speed = 100;
+
+            //2.2
+            Console.WriteLine($"car 1 \ncolor {car1.color}\nmodel {car1.model}\nspeed {car1.speed}Kmh");
+            Console.WriteLine("------------------");
+            //2.3
+            Console.WriteLine("you like to add speed prees 1\nor down speed prees 2");
+            choice = Convert.ToInt32(Console.ReadLine());
+            if (choice==1)
+            {
+                Console.WriteLine("how much?");
+                car1.speed += choice = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("------------------");
+            }
+            if (choice==2)
+            {
+                Console.WriteLine("how much?");
+                car1.speed -= choice = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("------------------");
+            }
+            //2.4
+            Console.WriteLine($"car 1 \ncolor {car1.color}\nmodel {car1.model}\nspeed {car1.speed}Kmh");
+            Console.WriteLine("------------------");
+            //2.5
+            car1.speed -= 2;
+            //2.6
+            Console.WriteLine($"car 1 \ncolor {car1.color}\nmodel {car1.model}\nspeed {car1.speed}Kmh");
+            Console.WriteLine("------------------");
+            //2.7.
+            Console.WriteLine("car stop");
+            car1.speed= 0;
+            Console.WriteLine($"car 1 \ncolor {car1.color}\nmodel {car1.model}\nspeed {car1.speed}Kmh");
+            Console.WriteLine("------------------");
+
+        }
+    }
+}
---
+++ C#/cookie homework.cs
@@ -0,0 +1,137 @@
+﻿using System;
+namespace ConsoleApp10
+{
+    class cookie
+    {
+        public int gluten = 12;
+        public int egg = 11;
+        public int sugar = 13;
+        public int flour = 14;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0, choice = 0;
+            cookie cookie1 = new cookie(), cookie2 = new cookie(), cookie3 = new cookie();
+            while (choice != 999)
+            {
+                Console.WriteLine("what you like to do? \n1)fill \n2)print \n3)update sugar on cookie1  \n4)Placement from first to second  \n5)update egg on cookie2 \nfor Exit 99");
+                Console.WriteLine("------------------------");
+                choice = Convert.ToInt32(Console.ReadLine());
+                if (choice == 1)
+                {
+                    fill(cookie1, cookie2);
+                }
+                if (choice == 2)
+                {
+                    Console.WriteLine("which cookie you want to print? (1/2) \nfor print all enter (3)");
+                    num = Convert.ToInt32(Console.ReadLine());
+                    if (num == 1)
+                    {
+                        PrintCooke(cookie1);
+                    }
+                    if (num == 2)
+                    {
+                        PrintCooke(cookie2);
+                    }
+                    if (num == 3)
+                    {
+                        PrintAllCooke(cookie1, cookie2);
+                    }
+                }
+                if (choice == 3)
+                {
+                    update_suger(cookie1);
+                }
+                if (choice==4)
+                {
+                    Placement(cookie1, cookie2);
+                }
+                if (choice==5)
+                {
+                    update_egg(cookie2);
+                }
+                if (choice == 99 )
+                {
+                    Console.WriteLine("Good day");
+                    break;
+                }
+                choice = 0;
+                System.Console.ReadKey();
+                Console.Clear();
+            }
+
+        }
+        static void fill(cookie cookie1, cookie cookie2)
+        {
+            Console.WriteLine("Enter a quantity of ingredients for coockie 1: (in grams)");
+            Console.WriteLine("gluten:");
+            cookie1.gluten = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("egg:");
+            cookie1.egg = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("sugar:");
+            cookie1.sugar = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("flour:");
+            cookie1.flour = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine($" gluten:{cookie1.gluten} \n egg: {cookie1.egg} \n sugar: {cookie1.sugar} \n flour: {cookie1.flour } ");
+
+
+            Console.WriteLine("Enter a quantity of ingredients for coockie 2: (in grams)");
+            Console.WriteLine("gluten:");
+            cookie2.gluten = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("egg:");
+            cookie2.egg = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("sugar:");
+            cookie2.sugar = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("flour:");
+            cookie2.flour = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($" gluten:{cookie2.gluten} \n egg: {cookie2.egg} \n sugar: {cookie2.sugar} \n flour: {cookie2.flour } ");
+        }
+
+        static void PrintCooke(cookie cookie)
+        {
+            Console.WriteLine($"Gluten:  |  {cookie.gluten}");
+            Console.WriteLine($"Egg:     |  {cookie.egg}");
+            Console.WriteLine($"Sugar:   |  {cookie.sugar}");
+            Console.WriteLine($"Flour:   |  {cookie.flour } ");
+        }
+
+        static void PrintAllCooke(cookie cookie1, cookie cookie2)
+        {
+            Console.WriteLine("cookie 1:");
+            Console.WriteLine($"Gluten:  |  {cookie1.gluten}");
+            Console.WriteLine($"Egg:     |  {cookie1.egg}");
+            Console.WriteLine($"Sugar:   |  {cookie1.sugar}");
+            Console.WriteLine($"Flour:   |  {cookie1.flour } ");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("cookie 2:");
+            Console.WriteLine($"Gluten:  |  {cookie2.gluten}");
+            Console.WriteLine($"Egg:     |  {cookie2.egg}");
+            Console.WriteLine($"Sugar:   |  {cookie2.sugar}");
+            Console.WriteLine($"Flour:   |  {cookie2.flour } ");
+        }
+
+        static void update_suger(cookie cookie1)
+        {
+            Console.WriteLine("update sugar on cookie1");
+            cookie1.sugar= Convert.ToInt32(Console.ReadLine());
+        }
+
+        static void Placement(cookie cookie1, cookie cookie2)
+        {
+            cookie2.egg = cookie1.egg;
+            cookie2.flour= cookie1.flour;
+            cookie2.gluten= cookie1.gluten;
+            cookie2.sugar = cookie1.sugar;
+        }
+
+        static void update_egg(cookie cookie2)
+        {
+            Console.WriteLine("update egg on cookie2");
+            cookie2.egg= Convert.ToInt32(Console.ReadLine());
+        }
+
+    }
+}
